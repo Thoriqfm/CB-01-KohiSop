7cb6e174023a8a1e992d6bfbaa294e89d1749c8d
Thoriqfm <tfarrelm@gmail.com>
2026-05-30 20:14:53 +0700

    fix: GUI Pembayaran, TODO: logic validate saldo

--- src/kohisop/gui/Pembayaran.java
+++ src/kohisop/gui/Pembayaran.java
@@ -278,7 +278,7 @@ public class Pembayaran extends javax.swing.JFrame {
         );
 
         pack();
-    }// </editor-fold>                        
+    }// </editor-fold>
 
     private void checkBoxTunaiActionPerformed(java.awt.event.ActionEvent evt) {
         uncheckOtherPaymentMethods(checkBoxTunai);
